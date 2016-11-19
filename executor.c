681c31357f01ccbf191f6de8600052cc75143af1
Alvin Zhu <alvinzhu33@yahoo.com>
2016-11-19 17:15:49 -0500

    Fixed sizeOf thing

--- executor.c
+++ executor.c
@@ -4,18 +4,22 @@
 #include <string.h>
 
 void main(){
+  char s1[256];
   char *s;
   char *array[50];
-  int i = 0;
+
   printf("OMG like wutchu want??? ");
-  fgets(s,sizeof(s)*4,stdin);
-  strtok(s,"\n");
+  fgets(s1,sizeof(s1),stdin);
+  strtok(s1,"\n");
+  s = s1;
+
+  int i = 0;
   while(s){
     array[i] = strsep(&s," ");
-    //printf("%s ",array[i]); 
+    //printf("%d: %s\n",i, array[i]);
     i++;
   }
-  
+
   array[i] = 0;
   execvp(array[0], array);
 }
